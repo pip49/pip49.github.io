296c61e3368cd57eff136b6e680d1c8fef5506c2
pip49 <hambyeongcheol44@gmail.com>
2025-09-17 12:09:34 +0900

    .

--- place.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-echo "Content-Type: text/plain"
-echo ""
-echo "현재 위치는 서울특별시 강남구입니다.더 넘어가지마 "
-echo ""
-echo "This information is provided ce.shadif."
-echo "내일 또만나요."
-echo " Good bye" 
